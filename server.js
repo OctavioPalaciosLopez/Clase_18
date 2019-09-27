fd21ddec99c550dd3141ddb9c706e7412d2f674d
unknown <ocpl2710@gmail.com>
2019-09-26 20:05:23 -0500

    Actividad 2

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express()
+const port = 9000
+
+
+const router = express.Router()
+
+router.get('/',(req,res) => {
+    res.sendFile(__dirname + '/public/index.html')
+})
+
+app.use('/asset', router)
+
+app.listen(9000, () => { 
+    console.log(`Example app listening on port ${port}!`)
+})
