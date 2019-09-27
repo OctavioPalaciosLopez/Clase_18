64adcf378fae5a17ef17403b373620750873de37
unknown <ocpl2710@gmail.com>
2019-09-26 20:56:46 -0500

    ACTIVIDAD 3

--- server.js
+++ server.js
@@ -5,8 +5,18 @@ const port = 9000
 
 const router = express.Router()
 
-router.get('/',(req,res) => {
+/*router.get('/',(req,res) => {
     res.sendFile(__dirname + '/public/index.html')
+})
+
+app.use('/asset', router)
+
+app.listen(9000, () => { 
+    console.log(`Example app listening on port ${port}!`)
+})*/
+
+router.get('/',(req,res) => {
+    res.sendFile(__dirname + '/index2.html')
 })
 
 app.use('/asset', router)
